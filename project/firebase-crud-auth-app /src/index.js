908fdc644909f7635f1d7b4f0dd4246e3d4aca6e
ethan6938 <en.hurwitz@gmail.com>
2025-06-10 09:57:10 +0200

    Update

--- project/firebase-crud-auth-app /src/index.js
+++ project/firebase-crud-auth-app /src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // ✅ React 18 API
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
